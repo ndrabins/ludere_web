ab0ddfb6fd3cbb7e796f252a472d3c6e2cf2a7c6
Noah Drabinsky <ndrabinsky@developertown.com>
2018-02-06 21:35:24 -0500

    added redux-persist

--- src/index.js
+++ src/index.js
@@ -6,8 +6,14 @@ import registerServiceWorker from "./registerServiceWorker";
 
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
+
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web and AsyncStorage for react-native
+import hardSet from "redux-persist/lib/stateReconciler/hardSet";
+import { PersistGate } from "redux-persist/integration/react";
+
 import ReduxThunk from "redux-thunk";
-import reducers from "./reducers";
+import rootReducer from "./reducers";
 
 import firebase from "firebase";
 
@@ -24,6 +30,13 @@ var config = {
 
 firebase.initializeApp(config);
 
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: hardSet
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const composeEnhancers =
   typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
@@ -34,12 +47,15 @@ const composeEnhancers =
 
 const enhancer = composeEnhancers(applyMiddleware(ReduxThunk));
 
-const store = createStore(reducers, enhancer);
+let store = createStore(persistedReducer, enhancer);
+let persistor = persistStore(store);
 
 // <App />
 ReactDOM.render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
