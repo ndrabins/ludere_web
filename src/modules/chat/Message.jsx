f837f11889102ae733614cfd76bc0d5542377254
Noah Drabinsky <noah.drabinsky1@gmail.com>
2018-06-18 12:11:15 -0400

    Can now write down markdown messages

--- src/modules/chat/Message.jsx
+++ src/modules/chat/Message.jsx
@@ -2,28 +2,39 @@ import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
 import Avatar from "@material-ui/core/Avatar";
-import { Typography } from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import Popover from "@material-ui/core/Popover";
+import MenuItem from "@material-ui/core/MenuItem";
+import ReactMarkdown from "react-markdown";
+import "./Message.css"; // this is here to override markdown css
 
 class Message extends Component {
   static propTypes = {
     classes: PropTypes.object.isRequired,
-    message: PropTypes.object.isRequired
+    message: PropTypes.object.isRequired,
+    type: PropTypes.string.isRequired,
+    formattedTimeStamp: PropTypes.string.isRequired
+  };
+
+  state = {
+    anchorEl: null,
+    hovered: false
   };
 
   renderNormalMessage = () => {
     const { classes, message, formattedTimeStamp } = this.props;
 
     return (
-      <div className={classes.messageContainer}>
+      <React.Fragment>
         <Avatar src={message.avatarURL} style={{ margin: "0px 10px" }} />
         <div className={classes.messageContent}>
           <div className={classes.messageHeader}>
             <div className={classes.name}>{message.sentByDisplayName}</div>
             <div className={classes.date}> {formattedTimeStamp} </div>
           </div>
-          <p className={classes.messageText}>{message.messageText}</p>
+          <ReactMarkdown source={message.messageText} />
         </div>
-      </div>
+      </React.Fragment>
     );
   };
 
@@ -31,43 +42,82 @@ class Message extends Component {
     const { classes, message, formattedTimeStamp } = this.props;
 
     return (
-      <div className={classes.messageContainer}>
+      <React.Fragment>
         <Avatar src={message.avatarURL} style={{ margin: "0px 10px" }} />
         <div className={classes.messageContent}>
           <div className={classes.messageHeader}>
             <div className={classes.name}>{message.sentByDisplayName}</div>
             <div className={classes.date}> {formattedTimeStamp} </div>
           </div>
-          <Typography variant="title" className={classes.messageText}>
-            {message.messageText}
-          </Typography>
+          <ReactMarkdown source={message.messageText} />
           <img
             src={message.fileURL}
             alt="file upload"
             className={classes.uploadedImage}
           />
         </div>
-      </div>
+      </React.Fragment>
     );
   };
   renderSmallMessage = () => {
     const { classes, message } = this.props;
     return (
-      <div className={classes.messageContainer}>
-        <div className={classes.messageBlockContent}>
-          <p className={classes.messageText}>{message.messageText}</p>
-        </div>
+      <div className={classes.messageBlockContent}>
+        <ReactMarkdown source={message.messageText} />
       </div>
     );
   };
 
+  handleMenuClick = event => {
+    event.stopPropagation();
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  handleMouseOver = () => {
+    this.setState({ hovered: true });
+  };
+  handleMouseLeave = () => {
+    this.setState({ hovered: false });
+  };
+
   render() {
     const { classes, type } = this.props;
+    const { anchorEl, hovered } = this.state;
     return (
-      <div className={classes.root}>
+      <div
+        className={classes.messageContainer}
+        onMouseOver={this.handleMouseOver}
+        onMouseLeave={this.handleMouseLeave}
+      >
         {type === "file" && this.renderFileMessage()}
         {type === "small" && this.renderSmallMessage()}
         {type === "normal" && this.renderNormalMessage()}
+        <MoreVertIcon
+          className={hovered ? classes.icon : classes.hiddenIcon}
+          aria-owns={anchorEl ? "simple-menu" : null}
+          aria-haspopup="true"
+          onClick={ev => this.handleMenuClick(ev)}
+        />
+        <Popover
+          open={Boolean(anchorEl)}
+          anchorEl={anchorEl}
+          onClose={this.handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center"
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center"
+          }}
+        >
+          <MenuItem> Edit Message </MenuItem>
+          <MenuItem> Delete Message </MenuItem>
+        </Popover>
       </div>
     );
   }
@@ -82,33 +132,39 @@ const styles = theme => ({
     flexDirection: "row",
     display: "flex",
     backgroundColor: "transparent",
+    alignItems: "center",
     transition: theme.transitions.create(["background-color"]),
     "&:hover": {
       backgroundColor: "#EEEEEE"
     }
   },
   messageContent: {
-    maxWidth: "90%",
-    marginRight: 30,
+    flex: 1,
     display: "flex",
-    flexDirection: "column"
+    flexDirection: "column",
+    whiteSpace: "pre-line",
+    wordWrap: "break-word",
+    wordBreak: "break-all",
+    overflowWrap: "break-word",
+    marginRight: 30,
+    marginTop: "0px",
+    fontSize: "15px"
   },
   messageBlockContent: {
-    width: "90%",
+    flex: 1,
     marginLeft: 60,
     display: "flex",
-    flexDirection: "column"
+    flexDirection: "column",
+    whiteSpace: "pre-line",
+    wordWrap: "break-word",
+    wordBreak: "break-all",
+    overflowWrap: "break-word",
+    marginTop: "0px",
+    fontSize: "15px"
   },
   messageHeader: {
     display: "flex",
     flexDirection: "row"
-  },
-  messageText: {
-    whiteSpace: "pre-line",
-    wordWrap: "break-word",
-    marginBottom: "3px",
-    marginTop: "0px",
-    fontSize: "15px"
   },
   name: {
     fontSize: 16,
@@ -120,6 +176,16 @@ const styles = theme => ({
     display: "flex",
     marginLeft: 3,
     color: "#b9bbbe"
+  },
+  icon: {
+    color: "#6f6f6f",
+    "&:hover": {
+      color: "#b9bbbe",
+      cursor: "pointer"
+    }
+  },
+  hiddenIcon: {
+    opacity: "0"
   }
 });
 export default withStyles(styles)(Message);
