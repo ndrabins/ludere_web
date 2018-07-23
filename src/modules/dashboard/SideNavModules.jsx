8a2089667d8039206d6cfafbe39aedd9103d9303
Noah Drabinsky <noah.drabinsky1@gmail.com>
2018-07-23 12:06:11 -0400

    Show user to click on a team when they first sign up / are invited

--- src/modules/dashboard/SideNavModules.jsx
+++ src/modules/dashboard/SideNavModules.jsx
@@ -5,7 +5,9 @@ import WorkflowSideNav from "../workflow/WorkflowSideNav";
 import { withStyles } from "@material-ui/core/styles";
 import { withRouter } from "react-router";
 import { Scrollbars } from "react-custom-scrollbars";
-// import SelectableTextButton from '../../common/SelectableTextButton';
+import Typography from "@material-ui/core/Typography";
+import ArrowIcon from "@material-ui/icons/ArrowRightAltRounded";
+import SelectTeamIcon from "static/undraw_selectTeam.svg";
 
 import TeamDashboard from "./TeamDashboardTitle";
 
@@ -20,13 +22,25 @@ class SideNavModules extends Component {
         <div className={classes.container}>
           {/* <Route exact path="/" component={CommunitySideNav} />
         <Route path="/community" component={CommunitySideNav} /> */}
-          {showModules && (
+          {showModules ? (
             <Fragment>
               <TeamDashboard />
 
               <ChatSideNav />
               <WorkflowSideNav />
             </Fragment>
+          ) : (
+            <div className={classes.teamNotSelected}>
+              <ArrowIcon className={classes.arrowIcon} />
+              <Typography align="center" className={classes.selectTeamText}>
+                Select or Create a team to start collaborating
+              </Typography>
+              <img
+                className={classes.icon}
+                src={SelectTeamIcon}
+                alt="Team icon"
+              />
+            </div>
           )}
         </div>
       </Scrollbars>
@@ -36,8 +50,9 @@ class SideNavModules extends Component {
 
 const styles = {
   container: {
-    marginTop: 10,
-    width: 240
+    paddingTop: 10,
+    width: 240,
+    height: "100%"
   },
   title: {
     display: "flex",
@@ -46,6 +61,33 @@ const styles = {
     alignItems: "center",
     height: 40,
     margin: "0px 10px 0 32px"
+  },
+  teamNotSelected: {
+    position: "relative",
+    display: "flex",
+    height: "100%",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flexDirection: "column"
+  },
+  icon: {
+    marginTop: 20,
+    minWidth: 100,
+    minHeight: 100,
+    maxHeight: 200,
+    maxWidth: 200
+  },
+  arrowIcon: {
+    position: "absolute",
+    color: "#C98BF1",
+    fontSize: 70,
+    left: "0",
+    top: "26px",
+    transform: "rotate(210deg)"
+  },
+  selectTeamText: {
+    color: "#FFFFFF",
+    fontSize: 18
   }
 };
 
